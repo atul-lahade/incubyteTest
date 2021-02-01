7860cdd3450614f76e14765267b33a7f288f6f14
atul2016 <atul.lahade2012@gmail.com>
2021-02-01 19:15:32 +0530

    Update StringCalculator.java
    added code to ignore numbers that are greater than 1000

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,11 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 class StringCalculator {
-	
-	public static void main(String args[]){
-		
-		System.out.println(add("3,-6,15,-18,46,33"));
-	}
 
 	public static int add(final String text) {
 
@@ -16,41 +11,41 @@ class StringCalculator {
 		String noWithoutDelimiter = text;
 		String[] stringArray = text.split(",|n");
 
-		if(stringArray.length == 0){
+		if (stringArray.length == 0) {
 			return sum;
 		}
-		
-		/*if (stringArray.length > 2) {
-			throw new RuntimeException("Upto 2 numbers separated by , are allowed");
-		}*/
-		
-	
+
+		/*
+		 * if (stringArray.length > 2) { throw new RuntimeException(
+		 * "Upto 2 numbers separated by , are allowed"); }
+		 */
+
 		if (text.startsWith("//")) {
 			int delimiterIndex = text.indexOf("//") + 2;
 			delimiter = text.substring(delimiterIndex, delimiterIndex + 1);
 			noWithoutDelimiter = text.substring(text.indexOf("n") + 1);
 			return add(noWithoutDelimiter, delimiter);
 		}
-				
-		if(stringArray.length > 0){
+
+		if (stringArray.length > 0) {
 			for (int i = 0; i < stringArray.length; i++) {
-				
+
 				if (!stringArray[i].isEmpty()) {
 					int numberInt = Integer.parseInt(stringArray[i]);
-					if(numberInt < 0){
+					if (numberInt < 0) {
 						negativeNumbers.add(Integer.parseInt(stringArray[i]));
+					}else if(numberInt <= 1000){
+						sum += numberInt;
 					}
-					sum += numberInt;
 				}
-				
 			}
 			if (negativeNumbers.size() > 0) {
-	            throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
-	        }
-			
+				throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+			}
+
 			return sum;
 		}
-				
+
 		return sum;
 		/*
 		 * } else { return 0; }
@@ -67,5 +62,11 @@ class StringCalculator {
 			}
 		}
 		return sum;
+	}
+
+	public int GetCalledCount() {
+		int count = 0;
+
+		return 0;
 	}
 }
