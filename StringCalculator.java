bbb07ae94efead09d4a9921b9d145ab9ff4b34a9
atul2016 <atul.lahade2012@gmail.com>
2021-02-01 18:02:37 +0530

    Update StringCalculator.java
    added code to handle new line between string

--- StringCalculator.java
+++ StringCalculator.java
@@ -3,13 +3,13 @@ class StringCalculator {
 
 	public static int add(final String text) {
 
-		String[] stringArray = text.split(",");
+		String[] stringArray = text.split(",|n");
 
-//		if (stringArray.length > 2) {
-//
-//			throw new RuntimeException("Upto 2 numbers separated by , are allowed");
-//		} else if (stringArray.length > 0) {
+/*		if (stringArray.length > 2) {
 
+			throw new RuntimeException("Upto 2 numbers separated by , are allowed");
+		} else if (stringArray.length > 0) {
+*/
 			int sum = 0;
 			for (int i = 0; i < stringArray.length; i++) {
 				if (stringArray[i] != "") {
@@ -17,9 +17,8 @@ class StringCalculator {
 				}
 			}
 			return sum;
-//		} else {
-//
-//			return 0;
-//		}
+/*		} else {
+			return 0;
+		}*/
 	}
 }
