7be5a3655b072ef82417252aeaa2741cda50fe5e
atul2016 <atul.lahade2012@gmail.com>
2021-02-01 18:12:24 +0530

    Update StringCalculator.java
    added code to support different types of delimiters

--- StringCalculator.java
+++ StringCalculator.java
@@ -5,20 +5,36 @@ class StringCalculator {
 
 		String[] stringArray = text.split(",|n");
 
-/*		if (stringArray.length > 2) {
+		/*
+		 * if (stringArray.length > 2) {
+		 * 
+		 * throw new RuntimeException(
+		 * "Upto 2 numbers separated by , are allowed"); } else if
+		 * (stringArray.length > 0) {
+		 */
+		String delimiter = ",|n";
+		String noWithoutDelimiter = text;
+		
+		if(text.startsWith("//")){
+			 int delimiterIndex = text.indexOf("//") + 2;
+			 delimiter = text.substring(delimiterIndex, delimiterIndex + 1);
+			 noWithoutDelimiter = text.substring(text.indexOf("n") + 1);
+		}
+		return add(noWithoutDelimiter, delimiter);
+		/*
+		 * } else { return 0; }
+		 */
+	}
 
-			throw new RuntimeException("Upto 2 numbers separated by , are allowed");
-		} else if (stringArray.length > 0) {
-*/
-			int sum = 0;
-			for (int i = 0; i < stringArray.length; i++) {
-				if (stringArray[i] != "") {
-					sum += Integer.parseInt(stringArray[i]);
-				}
-			}
-			return sum;
-/*		} else {
-			return 0;
-		}*/
+	private static int add(final String text, final String delimiter) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+	    String[] numbersArray = text.split(delimiter);
+	    for (String number : numbersArray) {
+	        if (!number.trim().isEmpty()) {
+	            sum += Integer.parseInt(number.trim());
+	        }
+	    }
+	    return sum;
 	}
 }
