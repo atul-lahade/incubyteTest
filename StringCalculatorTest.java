d3d1146851fcd8bf22500e488213bd6ccb2d7c16
atul2016 <atul.lahade2012@gmail.com>
2021-02-01 17:58:42 +0530

    Update StringCalculatorTest.java
    added test to handle unknown amount of numbers

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,14 +1,22 @@
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class StringCalculatorTest {
 
-	@Test(expected = RuntimeException.class)
-	public final void whenMoreThanTwoNumberUsed(){
-		StringCalculator.add("1,4,6,7");
-	}
+//	@Test(expected = RuntimeException.class)
+//	public final void whenMoreThanTwoNumberUsed(){
+//		StringCalculator.add("1,4,6,7");
+//	}
+//	
+//	@Test
+//	public final void whenStringIsEmpty(){
+//		Assert.assertEquals(0, StringCalculator.add(""));
+//	}
 	
 	@Test
-	public final void whenStringIsEmpty(){
-		Assert.assertEquals(0, StringCalculator.add(""));
-	}
+    public final void whenAnyNumberOfNumbersUsed() {
+        Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
+    }
 }
+
