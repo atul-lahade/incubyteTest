eb5d07fb467cb0b350d649404746b65f6cab8528
atul2016 <atul.lahade2012@gmail.com>
2021-02-01 18:34:45 +0530

    Update StringCalculator.java
    Added to handle all test cases together

--- StringCalculator.java
+++ StringCalculator.java
@@ -3,24 +3,37 @@ class StringCalculator {
 
 	public static int add(final String text) {
 
-		String[] stringArray = text.split(",|n");
-
-		/*
-		 * if (stringArray.length > 2) {
-		 * 
-		 * throw new RuntimeException(
-		 * "Upto 2 numbers separated by , are allowed"); } else if
-		 * (stringArray.length > 0) {
-		 */
+		int sum = 0;
 		String delimiter = ",|n";
 		String noWithoutDelimiter = text;
-		
-		if(text.startsWith("//")){
-			 int delimiterIndex = text.indexOf("//") + 2;
-			 delimiter = text.substring(delimiterIndex, delimiterIndex + 1);
-			 noWithoutDelimiter = text.substring(text.indexOf("n") + 1);
+		String[] stringArray = text.split(",|n");
+
+		if(stringArray.length == 0){
+			return sum;
 		}
-		return add(noWithoutDelimiter, delimiter);
+		
+		/*if (stringArray.length > 2) {
+			throw new RuntimeException("Upto 2 numbers separated by , are allowed");
+		}*/
+		
+	
+		if (text.startsWith("//")) {
+			int delimiterIndex = text.indexOf("//") + 2;
+			delimiter = text.substring(delimiterIndex, delimiterIndex + 1);
+			noWithoutDelimiter = text.substring(text.indexOf("n") + 1);
+			return add(noWithoutDelimiter, delimiter);
+		}
+				
+		if(stringArray.length > 0){
+			for (int i = 0; i < stringArray.length; i++) {
+				if(!stringArray[i].isEmpty()){
+					sum += Integer.parseInt(stringArray[i]);
+				}	
+			}
+			return sum;
+		}
+		
+		return sum;
 		/*
 		 * } else { return 0; }
 		 */
@@ -29,12 +42,12 @@ class StringCalculator {
 	private static int add(final String text, final String delimiter) {
 		// TODO Auto-generated method stub
 		int sum = 0;
-	    String[] numbersArray = text.split(delimiter);
-	    for (String number : numbersArray) {
-	        if (!number.trim().isEmpty()) {
-	            sum += Integer.parseInt(number.trim());
-	        }
-	    }
-	    return sum;
+		String[] stringArray = text.split(delimiter);
+		for (int i = 0; i < stringArray.length; i++) {
+			if (!stringArray[i].isEmpty()) {
+				sum += Integer.parseInt(stringArray[i].trim());
+			}
+		}
+		return sum;
 	}
 }
