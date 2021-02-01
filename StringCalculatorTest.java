7b0cd0a0cf2eadfd5bea169fc34d55072e72debb
atul2016 <atul.lahade2012@gmail.com>
2021-02-01 18:03:32 +0530

    Update StringCalculatorTest.java
    added test case to handle new line between numbers

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -4,19 +4,24 @@ import junit.framework.Assert;
 
 public class StringCalculatorTest {
 
-//	@Test(expected = RuntimeException.class)
-//	public final void whenMoreThanTwoNumberUsed(){
-//		StringCalculator.add("1,4,6,7");
-//	}
-//	
-//	@Test
-//	public final void whenStringIsEmpty(){
-//		Assert.assertEquals(0, StringCalculator.add(""));
-//	}
+	/*@Test(expected = RuntimeException.class)
+	public final void whenMoreThanTwoNumberUsed(){
+		StringCalculator.add("1,4,6,7");
+	}
+	
+	@Test
+	public final void whenStringIsEmpty(){
+		Assert.assertEquals(0, StringCalculator.add(""));
+	}
 	
 	@Test
     public final void whenAnyNumberOfNumbersUsed() {
         Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
-    }
+    }*/
+	
+	@Test
+	public final void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums() {
+	    Assert.assertEquals(9+2+5, StringCalculator.add("9,2n5"));
+	}
 }
 
